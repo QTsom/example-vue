832e2d3683e48f41c7e52abb7457bc74bac0d1f9
QTsom <somin5998@gmail.com>
2023-09-21 12:33:13 +0900

    feat: edit app.vue

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,40 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import Content from "./components/Content.vue"
+  import Change from "./components/Change.vue"
+  import CountEvent from "./components/CountEvent.vue"
+  import MinusCountEvent from "./components/MinusCountEvent.vue"
+  import MessageForm from "./components/MessageForm.vue"
+  import CheckForm from "./components/CheckForm.vue"
+  import Chart from "./components/Chart.vue"
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div>
+    <h1 class="title">HELLO</h1>
+    
+    <div>
+      <Content />
+      
+      <Change />
+      
+      <CountEvent />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <MinusCountEvent />
+      
+      <MessageForm />
+      
+      <CheckForm />
+
+      <Chart />
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+<style scope>
+  .title {
+    font-size: 30px;
+    font-weight: 800;
+    text-align: center;
+    text-decoration: underline;
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
