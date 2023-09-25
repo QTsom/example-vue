b7b7fd222a3a491b5d508bc4a0c065075ebcf899
QTsom <somin5998@gmail.com>
2023-09-25 15:09:52 +0900

    feat: add transition file

---
+++ src/components/Transition.vue
@@ -0,0 +1,26 @@
+<script setup>
+    import {ref} from "vue"    
+
+    const show = ref(true)
+</script>
+
+<template>
+    <div>
+        <button @click="show = !show">토글 버튼</button>
+        <Transition>
+            <p v-if="show">안녕</p>
+        </Transition>
+    </div>
+</template>
+
+<style scoped>    
+    .v-enter-active,
+    .v-leave-active {
+    transition: opacity 0.5s ease;
+    }
+
+    .v-enter-from,
+    .v-leave-to {
+    opacity: 0;
+    }
+</style>
