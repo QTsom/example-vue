a6e16136c3586a4c4d47b6033f350ecef71f99f9
QTsom <somin5998@gmail.com>
2023-11-28 15:38:59 +0900

    design: edit toggle button css

--- src/components/TransitionEvent.vue
+++ src/components/TransitionEvent.vue
@@ -8,24 +8,34 @@
     <div class="wrapper">
         <button @click="show = !show" class="toggle-button">토글 버튼</button>
         <Transition>
-            <p v-if="show">위에 버튼 누르면 텍스트 사라짐</p>
+            <p v-if="show" class="toggle-text">위에 버튼 누르면 텍스트 사라짐</p>
         </Transition>
     </div>
 </template>
 
 <style scoped>    
     .wrapper  {
+        height: 72px;
         text-align: center;
     }
     .toggle-button {
-        height: 35px;
+        height: 30px;
         padding: 0 15px;
+        margin-bottom: 40px;
         margin-bottom: 10px;
         color: #f1f1f1;
-        font-size: 20px;
+        font-size: 18px;
         border: none;
         border-radius: 15px;
         background: rgb(237, 146, 146);
+        cursor: pointer;
+        transition: 0.5s;
+    }
+    .toggle-button:hover {
+        background: rgb(178, 178, 241);
+    }
+    .toggle-text {
+        font-size: 20px;
     }
     .v-enter-active,
     .v-leave-active {
